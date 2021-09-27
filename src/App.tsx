81443ec89e63fc5db1b8a4b1b24108529723bff8
Mihran <xachatryan200104@gmail.com>
2021-09-28 00:08:49 +0400

    1

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
+
+//1. Version of infinite loop
+
+// function App() {
+//   const [name, setName] = useState("");
+//   const [renderCount, setRenderCount] = useState(0);
+
+//   useEffect(() => {
+//     setRenderCount((prevRenderCount) => prevRenderCount + 1);
+//   });
+
+//   return (
+//     <>
+//       <input value={name} onChange={(e) => setName(e.target.value)} />
+//       <div>My name is {name}</div>
+//       <div>I rendered {renderCount} times</div>
+//     </>
+//   );
+// }
+
+//2.Right way with useRef
+
+// function App() {
+//   const [name, setName] = useState("");
+//   const renderCount = useRef(1);
+//   useEffect(() => {
+//     renderCount.current = renderCount.current + 1;
+//   });
+//   return (
+//     <>
+//       <input type={name} onChange={(e) => setName(e.target.value)} />
+//       <div>My name is {name}</div>
+//       <div>I rendered {renderCount.current}</div>
+//     </>
+//   );
+// }
+
+export default App;
